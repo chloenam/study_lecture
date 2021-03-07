fef97831a3479c7e486d755a2f6ad2fc3de61302
chloe <maid-s@naver.com>
2021-03-07 22:44:27 +0900

    Update index.js
    if문 사용

--- js_zerocho/gugudan/index.js
+++ js_zerocho/gugudan/index.js
@@ -4,3 +4,20 @@
   const first = document.querySelector("#first").value;
   const second = document.querySelector("#second").value;
   const result = document.querySelector("#result");
+
+  //a,b값 찾기
+  if (first) {
+    //yes
+    if (second) {
+      //yes
+
+      const play = first * second;
+      result.textContent = play;
+    } else {
+      //no
+      result.textContent = "두번째 값 입력해주세요";
+    }
+  } else {
+    //no
+    result.textContent = "첫번째 값 입력해주세요";
+  }
