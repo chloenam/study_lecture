4be7ec5e9f0bee43a8aef855f04a801e7b952deb
chloe <maid-s@naver.com>
2021-03-07 22:43:52 +0900

    Update index.js
    변수 선언

--- js_zerocho/gugudan/index.js
+++ js_zerocho/gugudan/index.js
@@ -1,2 +1,6 @@
 // console.log("hello world");
 // document.write("hello world");
+  //변수선언(저장)
+  const first = document.querySelector("#first").value;
+  const second = document.querySelector("#second").value;
+  const result = document.querySelector("#result");
