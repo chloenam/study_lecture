8ece796e63530cb4a903fbe02f478d27c3f9703e
chloe <maid-s@naver.com>
2021-03-09 23:10:33 +0900

    Update variable.js
    variable >let  new / var old

--- js_note_dreamcoding-ellie/variable.js
+++ js_note_dreamcoding-ellie/variable.js
@@ -2,3 +2,26 @@
 // added in ES 5
 // use this for Vanila Javascript.
 "use strict";
+
+// 2. Variable, rw(read/write)
+// let (added in ES6)
+let globalName = "global name";
+{
+  let name = "ellie";
+  console.log(name);
+  name = "hello";
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+{
+  age = 4;
+  var age;
+}
+console.log(age);
+
