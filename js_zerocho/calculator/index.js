873bbb04298cb4c59512b8b7eede00d978b37ada
chloe <maid-s@naver.com>
2021-03-11 23:19:21 +0900

    Update index.js
    add variable

--- js_zerocho/calculator/index.js
+++ js_zerocho/calculator/index.js
@@ -6,3 +6,8 @@ const divideBtn = document.querySelector("#divide");
 const multiplyBtn = document.querySelector("#multiply");
 const calculateBtn = document.querySelector("#calculate");
 const resultInput = document.querySelector("#result");
+
+//고정값이 아닌 바뀌는 값이므로 let으로 선언
+let temp;
+let operator;
+
